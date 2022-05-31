bf6fe3cb434306f953b89c96b09fc29cbbc101b4
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2022-05-31 20:56:26 +1000

    Create CRUD code for mall-ware, mall-coupon, mall-member, and mall-order module used renren-generator and MyBatis-Plus.

--- mall/mall-coupon/src/test/java/com/sv/mall/coupon/MallCouponApplicationTests.java
+++ mall/mall-coupon/src/test/java/com/sv/mall/coupon/MallCouponApplicationTests.java
@@ -1,9 +1,13 @@
 package com.sv.mall.coupon;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=MallCouponApplication.class)
 class MallCouponApplicationTests {
 
 	@Test
--- mall/mall-ware/src/test/java/com/sv/mall/ware/MallWareApplicationTests.java
+++ mall/mall-ware/src/test/java/com/sv/mall/ware/MallWareApplicationTests.java
@@ -2,9 +2,12 @@ package com.sv.mall.ware;
 
 import org.junit.Test;
 //import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=MallWareApplication.class)
 class MallWareApplicationTests {
 
     @Test
